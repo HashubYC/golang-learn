7ad9cd323115f31092d160f32f1d3a7e08905147
HashubYC <1055757507@qq.com>
2023-05-28 23:18:17 +0800

    知识点: nil 赋值

---
+++ src/question_daily/day_2023_5/20230528.go
@@ -0,0 +1,20 @@
+package day_2023_05
+
+import "fmt"
+
+// 下面赋值正确的是
+
+func Day_2023_05_28() {
+	// var x = nil
+	// var x interface{} = nil
+	// var x string = nil
+	var x error = nil
+
+	fmt.Println(x)
+
+	/**
+	BD
+	知识点是 nil
+	nil 只能赋值给指针、chan、func、interface、map 或 slice 类型的变量。
+	*/
+}
