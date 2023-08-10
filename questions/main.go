c52f5a814499361bb51af37eeee48ebbb04e20f1
HashubYC <1055757507@qq.com>
2023-08-10 16:23:17 +0800

    slice is a pointer

--- questions/main.go
+++ questions/main.go
@@ -2,10 +2,14 @@ package main
 
 import (
 	// question "questions/question"
-	day_2023_07 "questions/question_daily/day_2023_7"
+	day_2023_08 "questions/question_daily/day_2023_8"
 )
 
 func main() {
+
+	// day_2023_08.Day_2023_08_03()
+	// day_2023_08.Day_2023_08_04()
+	day_2023_08.Day_2023_08_10()
 
 	// day_2023_07.Day_2023_07_13()
 	// day_2023_07.Day_2023_07_14()
@@ -13,7 +17,7 @@ func main() {
 	// day_2023_07.Day_2023_07_25()
 	// day_2023_07.Day_2023_07_26()
 	// day_2023_07.Day_2023_07_28()
-	day_2023_07.Day_2023_07_31()
+	// day_2023_07.Day_2023_07_31()
 
 	// question.Test()
 
