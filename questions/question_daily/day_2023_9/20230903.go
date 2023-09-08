1cc2fac3a956d4b34af427373fe992ac3a0c76fa
HashubYC <1055757507@qq.com>
2023-09-08 09:49:38 +0800

    位运算只能操作整数

---
+++ questions/question_daily/day_2023_9/20230903.go
@@ -0,0 +1,12 @@
+package day_2023_09
+
+// | 操作是按位或操作符，它的操作数只能是整数
+// Go 规范的内容 https://docs.studygolang.com/ref/spec#Arithmetic_operators：
+
+import "fmt"
+
+func Day_2023_09_03() {
+	var a, b float64 = 1.0, 4.0
+	// fmt.Println(a | b)
+	fmt.Println(a, b)
+}
